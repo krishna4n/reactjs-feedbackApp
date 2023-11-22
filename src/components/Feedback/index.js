7968af5fdd06e468ea1b7fef85f53fb6ebecbadb
Gopikrishna <45033153+krishna4n@users.noreply.github.com>
2023-11-22 14:36:16 +0530

    Update index.js

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,66 +1,114 @@
-import {Component} from 'react'
-import './index.css'
+.app-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to right, #ffeeee, #ffc0bb);
+  height: 100vh;
+}
 
-class Feedback extends Component {
-  state = {hasFeedBack: false}
+.feedback-card {
+  background-color: #ffffff;
+  width: 80%;
+  border-radius: 12px;
+  max-width: 606px;
+  padding: 24px;
+}
 
-  selectedEmoji = () => {
-    this.setState({
-      hasFeedBack: true,
-    })
-  }
-
-  render() {
-    let message = ''
-    const {hasFeedBack} = this.state
-    const {resources} = this.props
-    const {emojis} = resources
-    if (hasFeedBack === false) {
-      message = '"How satisfied are you with our customer support performance?'
-    }
-
-    return (
-      <div className="container">
-        <div className="card-container">
-          <h1 className="heading">{message}</h1>
-          <div className="feedback-container">
-            {!hasFeedBack &&
-              emojis.map(eachItem => (
-                <ul className="emoji" key={eachItem.id}>
-                  <li>
-                    <img
-                      src={eachItem.imageUrl}
-                      alt={eachItem.name}
-                      className="emoji-icon"
-                      onClick={this.selectedEmoji}
-                    />
-                  </li>
-                  <li>{eachItem.name}</li>
-                </ul>
-              ))}
-            {hasFeedBack && (
-              <ul className="emoji">
-                <li>
-                  <img
-                    src={resources.loveEmojiUrl}
-                    alt="love emoji"
-                    className="emoji-icon"
-                  />
-                </li>
-                <li>
-                  <h4>Thank You!</h4>
-                </li>
-                <li>
-                  We will use your feedback to improve our customer support
-                  performance
-                </li>
-              </ul>
-            )}
-          </div>
-        </div>
-      </div>
-    )
+@media screen and (min-width: 768px) {
+  .feedback-card {
+    padding: 32px;
   }
 }
 
-export default Feedback
+@media screen and (min-width: 992px) {
+  .feedback-card {
+    padding: 64px;
+  }
+}
+
+.feedback-question-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.feedback-question {
+  text-align: center;
+  color: #0f172a;
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: 500;
+}
+
+@media screen and (min-width: 992px) {
+  .feedback-question {
+    font-size: 32px;
+  }
+}
+
+.emojis-list {
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+  margin-top: 32px;
+  padding: 0px;
+}
+
+.emoji-btn {
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+  outline: none;
+}
+
+.emoji {
+  width: 45px;
+  height: 45px;
+  margin-bottom: 16px;
+}
+
+@media screen and (min-width: 768px) {
+  .emoji {
+    width: 70px;
+    height: 70px;
+  }
+}
+
+.emoji-name {
+  color: #1e293b;
+  font-family: 'Roboto';
+  font-size: 16px;
+}
+
+@media screen and (min-width: 768px) {
+  .emoji-name {
+    font-size: 12px;
+  }
+}
+
+.thank-you-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.love-emoji {
+  width: 60px;
+  height: 54px;
+}
+
+.thank-you-text {
+  text-align: center;
+  color: #0f172a;
+  font-family: 'Roboto';
+  font-size: 24px;
+  font-weight: 500;
+}
+
+.description {
+  text-align: center;
+  color: #0f172a;
+  font-family: 'Roboto';
+  font-size: 16px;
+  font-weight: 300;
+}
